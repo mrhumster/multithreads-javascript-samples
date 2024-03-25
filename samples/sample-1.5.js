09c73822c90085c6d7ee5ab2e12330ffb760f4ce
XOMRKOB <ru89050995432@yandex.ru>
2024-03-25 09:39:00 +0600

    init commit

---
+++ samples/sample-1.5.js
@@ -0,0 +1,5 @@
+const vm = require('vm')
+const ContextObject = vm.runInNewContext('Object')
+console.log(Object === ContextObject)
+console.log(new Object() instanceof ContextObject)
+console.log(ContextObject.name)
